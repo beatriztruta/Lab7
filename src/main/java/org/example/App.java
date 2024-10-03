c4cb97c038023c5bd61c8612f4599f08bae2ca34
welbber <welbervital01@gmail.com>
2024-10-03 11:50:17 -0300

    Ajuste das depdendencias do slf4j e criacao teste basico na main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,40 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.model.Cliente;
+import org.example.model.Pedido;
+import org.example.model.Produto;
+import org.example.queue.Estoque;
+import org.example.queue.FilaDePedidos;
+import org.example.schedule.Reabastecedor;
+import org.example.schedule.RelatorioDeVendas;
+import org.example.worker.Worker;
 
-public class App
-{
-    public static void main( String[] args ) {
-        System.out.println("Hello World");
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        Estoque estoque = new Estoque();
+        FilaDePedidos filaDePedidos = new FilaDePedidos(10);
+
+        ExecutorService workers = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 4; i++) {
+            workers.submit(new Worker(filaDePedidos, estoque));
+        }
+
+        Reabastecedor.iniciarReabastecimento(estoque);
+        RelatorioDeVendas.iniciarRelatorio(estoque);
+
+        Produto[] produtos1 = {new Produto("Produto A", 2), new Produto("Produto B", 1)};
+        Produto[] produtos2 = {new Produto("Produto A", 3)};
+
+        try {
+            filaDePedidos.adicionarPedido(new Pedido(new Cliente(1), produtos1));
+            filaDePedidos.adicionarPedido(new Pedido(new Cliente(2), produtos2));
+        } catch (InterruptedException e) {
+            log.error("Houve um erro durante executado do projeto ", e);
+        }
     }
 }
